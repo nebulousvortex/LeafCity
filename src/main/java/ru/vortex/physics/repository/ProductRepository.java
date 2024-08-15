53df9b938b702bb276d8333f68aba48d466ae247
Слобожанин Федор <fslob@getcrm.ru>
2024-08-15 23:05:15 +0300

    Minor fixes

--- src/main/java/ru/vortex/physics/repository/ProductRepository.java
+++ src/main/java/ru/vortex/physics/repository/ProductRepository.java
@@ -6,6 +6,4 @@ import ru.vortex.physics.model.shop.Product;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findById (int id);
-
 }
