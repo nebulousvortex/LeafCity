fcd73644ead8f42759df6a21e946a95afdd2b897
Слобожанин Федор <fslob@getcrm.ru>
2024-08-15 14:38:35 +0300

    Minor changes

--- src/main/java/ru/vortex/physics/service/PaymentService.java
+++ src/main/java/ru/vortex/physics/service/PaymentService.java
@@ -10,6 +10,7 @@ import ru.vortex.physics.model.Payment;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
+import java.util.Random;
 
 @Service
 public class PaymentService {
@@ -18,7 +19,7 @@ public class PaymentService {
         RestTemplate restTemplate = new RestTemplate();
         String shopId = System.getenv("SHOP_ID");
         String secretKey = System.getenv("SECRET_KEY");
-        String IdempotenceKey = "keykeykock";
+        String IdempotenceKey = getRandomString();
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -31,5 +32,19 @@ public class PaymentService {
         HttpEntity<Payment> request = new HttpEntity<>(payment, headers);
 
         return restTemplate.postForObject("https://api.yookassa.ru/v3/payments", request, Payment.class);
+    }
+
+    private String getRandomString(){
+        int leftLimit = 97;
+        int rightLimit = 122;
+        int targetStringLength = 8;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        return buffer.toString();
     }
 }
