e786e07e2d9e42178a3a552cb4b1193841563920
Слобожанин Федор <fslob@getcrm.ru>
2024-08-15 15:47:41 +0300

    Fixes JSON token

--- src/main/java/ru/vortex/physics/controller/PaymentController.java
+++ src/main/java/ru/vortex/physics/controller/PaymentController.java
@@ -1,11 +1,15 @@
 package ru.vortex.physics.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.vortex.physics.model.Amount;
 import ru.vortex.physics.model.Confirmation;
 import ru.vortex.physics.model.Payment;
 import ru.vortex.physics.service.PaymentService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController()
 @RequestMapping("/api/v1")
@@ -28,7 +32,7 @@ public class PaymentController {
 
     @PostMapping("/getToken")
     @ResponseBody
-    public String getConfirmationToken() {
+    public ResponseEntity<Map<String, String>> getConfirmationToken() {
         Payment newPay = new Payment();
         newPay.setAmount(new Amount("1", "RUB"));
         newPay.setDescription("Тестовое описание");
@@ -36,6 +40,9 @@ public class PaymentController {
         newPay.setConfirmation(new Confirmation("embedded"));
 
         newPay = paymentService.createPayment(newPay);
-        return newPay.getConfirmation().getConfirmation_token();
+        Map<String, String> response = new HashMap<>();
+        response.put("confirmation_token", newPay.getConfirmation().getConfirmation_token());
+
+        return ResponseEntity.ok(response);
     }
 }
