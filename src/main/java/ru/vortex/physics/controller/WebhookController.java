fd2ddaf46c80de4ace1742db0c38a075402e2528
Слобожанин Федор <fslob@getcrm.ru>
2024-08-16 23:32:50 +0300

    Minor changes

--- src/main/java/ru/vortex/physics/controller/WebhookController.java
+++ src/main/java/ru/vortex/physics/controller/WebhookController.java
@@ -3,6 +3,8 @@ package ru.vortex.physics.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.vortex.physics.model.payment.Confirmation;
+import ru.vortex.physics.model.payment.Payment;
 import ru.vortex.physics.model.payment.Webhook;
 import ru.vortex.physics.service.PaymentService;
 
@@ -18,9 +20,9 @@ public class WebhookController {
     @ResponseBody
     public ResponseEntity<String> getNotify(@RequestBody Webhook webhook) {
         if(webhook.getObject() != null){
-            paymentService.savePayment(webhook.getObject());
-            System.out.println(webhook.getObject().getDescription());
-            System.out.println(webhook.getObject().getStatus());
+            Payment payment =  webhook.getObject();
+            payment.getConfirmation().setEnforce(true);
+            paymentService.savePayment(payment);
         }
         return ResponseEntity.ok("Ok");
     }
