9ba13f7d00dd988021dfca609da39c38e0d71059
Слобожанин Федор <fslob@getcrm.ru>
2024-08-18 23:31:29 +0300

    Added getting las payments by days

--- src/main/java/ru/vortex/physics/controller/PaymentController.java
+++ src/main/java/ru/vortex/physics/controller/PaymentController.java
@@ -12,9 +12,10 @@ import ru.vortex.physics.model.shop.Product;
 import ru.vortex.physics.service.PaymentService;
 import ru.vortex.physics.service.ShopService;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @RestController()
 @RequestMapping("/payment")
@@ -54,5 +55,50 @@ public class PaymentController {
     @ResponseBody
     public Payment getPayment(@RequestParam  String id) {
         return paymentService.getPayment(id);
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/getLastPayments")
+    @ResponseBody
+    public List<Map<String, Object>> getLastPayments() {
+        List<Payment> allPayments = paymentService.getPayments();
+        allPayments.sort(Comparator.comparing(Payment::getCreated_at).reversed());
+        List<Payment> lastPayments = allPayments.subList(0, Math.min(10, allPayments.size()));
+
+        List<Map<String, Object>> jsonResults = new ArrayList<>();
+
+        for (Payment payment : lastPayments) {
+            Map<String, Object> paymentJson = new HashMap<>();
+
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+            formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+            Date createdDate = null;
+            try {
+                createdDate = formatter.parse(payment.getCreated_at());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            Long diffInMillis = System.currentTimeMillis() - createdDate.getTime();
+            Long diffDays = TimeUnit.DAYS.convert(diffInMillis, TimeUnit.MILLISECONDS);
+            String daysAgo ="";
+            if (diffDays==0){
+                daysAgo = "сегодня!";
+            }else if (diffDays.toString().endsWith("1")){
+                daysAgo = diffDays + "день назад";
+            }
+            else if (diffDays.toString().endsWith("2")||diffDays.toString().endsWith("3")||diffDays.toString().endsWith("4")){
+                daysAgo = diffDays + "дня назад";
+            }else{
+                daysAgo = diffDays + "дней назад";
+            }
+
+            paymentJson.put("daysAgo", daysAgo);
+            paymentJson.put("username", payment.getMetadata().getUsername());
+            paymentJson.put("product_id", payment.getMetadata().getProductId());
+            jsonResults.add(paymentJson);
+        }
+
+        return jsonResults;
     }
 }
