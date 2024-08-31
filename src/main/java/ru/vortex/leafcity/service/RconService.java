64d17084148d29b03c0994edd071e1e569151c9e
Слобожанин Федор <fslob@getcrm.ru>
2024-08-31 18:29:01 +0300

    added amount field to notify

--- src/main/java/ru/vortex/leafcity/service/RconService.java
+++ src/main/java/ru/vortex/leafcity/service/RconService.java
@@ -18,20 +18,22 @@ public class RconService {
     RemoteConsole remoteConsole;
 
     public void sendCommand (String username, Product product) throws IOException, AuthenticationException {
-        String result;
         String duration = "";
-        Rcon rcon = new Rcon(
-                remoteConsole.getIp(),
-                Integer.parseInt(remoteConsole.getLobbyPort()),
-                remoteConsole.getLobbyPass().getBytes()
-        );
-        if(product.getDuration().getDuration()>0){
-            duration = " " + product.getDuration().getDuration() + "d";
+        if (product!= null){
+            Rcon rcon = new Rcon(
+                    remoteConsole.getIp(),
+                    Integer.parseInt(remoteConsole.getLobbyPort()),
+                    remoteConsole.getLobbyPass().getBytes()
+            );
+            if(product.getDuration().getDuration()>0){
+                duration = " " + product.getDuration().getDuration() + "d";
+            }
+            if(!(product.getCommand() == null) && !(product.getCommand().trim().isEmpty())) {
+                String command = product.getCommand().replace("{username}", username) + duration;
+                rcon.command(command);
+            }
+            sendNotify(username, product.getNotify().replace("{amount}", String.valueOf(product.getRealPrice())));
         }
-        String command = product.getCommand().replace("{username}", username) + duration;
-        result = rcon.command(command);
-        sendNotify(username, product.getNotify());
-
     }
 
     private void sendNotify (String username,String notifyMessage) throws IOException, AuthenticationException {
