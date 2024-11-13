dbf4dc63b8540f48300478b286d7046f607ef71e
Andrew <fylhtqhecfrjd@gmail.com>
2024-11-13 13:01:06 +0300

    Add log

--- src/main/java/ru/vortex/leafcity/controller/WebhookController.java
+++ src/main/java/ru/vortex/leafcity/controller/WebhookController.java
@@ -47,6 +47,7 @@ public class WebhookController {
                 if (payment.getStatus().equals("succeeded")) {
                     var product = shopService.getProductById(payment.getMetadata().getProductId());
                     product.setRealPrice(Float.parseFloat(webhook.getObject().getAmount().getValue()));
+                    System.out.println(product.getRealPrice() + "   " + webhook.getObject().getAmount().getValue());
                     var username = payment.getMetadata().getUsername();
                     RestTemplate restTemplate = new RestTemplate();
                     HttpHeaders headers = new HttpHeaders();
